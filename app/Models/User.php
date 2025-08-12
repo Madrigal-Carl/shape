ba86ec72a0b58cebb486b173e022ccd3d4c3120d
Madrigal-Carl <carlsalido.madrigal@gmail.com>
2025-08-12 14:41:16 +0800

    added connection to firestore database

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,47 +2,53 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
+use Roddy\FirestoreEloquent\Facade\FModel;
 
-class User extends Authenticatable
+class User extends FModel
 {
-    /** @use HasFactory<\Database\Factories\UserFactory> */
-    use HasFactory, Notifiable;
+    /**
+    * Name of your firestore collection
+    */
+    protected $collection = 'users';
 
     /**
-     * The attributes that are mass assignable.
-     *
-     * @var list<string>
-     */
+    * The primary key of the model/collection
+    */
+    protected $primaryKey = 'id';
+
+    /**
+    * The fillable property takes care of defining which fields are
+    * to be considered when the user will insert or update data.
+    * Fillable property should ba an array. e.g ['id', 'age', 'name']
+    */
     protected $fillable = [
-        'name',
-        'email',
+        'username',
         'password',
     ];
 
     /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var list<string>
-     */
+    * The required property takes care of defining which fields
+    * should be required and cannot be empty when inserting data only.
+    * Required property should ba an array. e.g ['id', 'name']
+    */
+    protected $required = [];
+
+    /**
+    * The default property is use to set a default value for
+    * the fields provided in the array if the fields is empty.
+    * Default property should ba an array. e.g ['name' => 'Alfred', 'date' => date('Y-m-d)]
+    */
+    protected $default = [];
+
+    /**
+    * The fieldTypes property is use to set a types for
+    * the fields provided in the array.
+    * Supported types are: integer, string, array and object
+    * fieldTypes property should ba an array. e.g ['name' => 'string', 'age' => 'int', 'date' => 'date']
+    */
+    protected $fieldTypes = [];
+
     protected $hidden = [
         'password',
-        'remember_token',
     ];
-
-    /**
-     * Get the attributes that should be cast.
-     *
-     * @return array<string, string>
-     */
-    protected function casts(): array
-    {
-        return [
-            'email_verified_at' => 'datetime',
-            'password' => 'hashed',
-        ];
-    }
 }
